7c54fda496b08d589376cf4089eff129c20be222
Curio <rayen159nasraoui@outlook.com>
2025-08-01 13:11:09 +0100

    added clone and recording system

---
+++ scripts/RecordingController.gd
@@ -0,0 +1,63 @@
+extends PlatformerController2D
+
+class_name RecordingController
+
+# Signal to notify clones when recording is complete
+signal recording_finished(recording_data: Dictionary)
+
+var count = 0
+var is_recording = false
+var recording_timer = 0.0
+
+#Our dictionary we store values too
+var save_data = {"0": ["nothing",Vector2(0,0),false]}
+
+var motion = Vector2.ZERO
+var UP : Vector2 = Vector2(0,-1)
+
+var currentClone : Clone
+@onready var ani = $AnimatedSprite2D
+
+##Period for recording player's actions in seconds
+@export_range(1, 10) var recordingDuration: float = 3.0
+##How often to record data per second
+@export_range(10, 60) var recordingFrequency: float = 30.0
+
+func _ready():
+	super._ready()
+	# Add player to group so clones can find it
+	add_to_group("player")
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	
+	if Input.is_action_just_pressed("record") and not is_recording:
+		start_recording()
+	
+	if is_recording:
+		recording_timer += delta
+		
+		# Record player state at the specified frequency
+		if count % int(60.0 / recordingFrequency) == 0:
+			count += 1
+			save_data[str(count)] = [ani.animation, global_position, ani.flip_h]
+		
+		# Stop recording after the duration
+		if recording_timer >= recordingDuration:
+			stop_recording()
+
+func start_recording():
+	is_recording = true
+	recording_timer = 0.0
+	count = 0
+	save_data.clear()
+	save_data["0"] = ["nothing", Vector2(0,0), false]
+	print("Recording started...")
+
+func stop_recording():
+	is_recording = false
+	print("Recording finished! Sending data to clone...")
+	recording_finished.emit(save_data)
+	
+	# Reset for next recording
+	recording_timer = 0.0
---
+++ scripts/clone/Clone.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+class_name Clone
+
+var load_data : Dictionary = Dictionary()
+var count = 0
+var is_playing = false
+var playback_timer = 0.0
+
+@onready var ani = $AnimatedSprite2D
+
+func _ready():
+	# Connect to the player's recording controller signal
+	var player = get_tree().get_first_node_in_group("player")
+	if player and player.has_signal("recording_finished"):
+		player.recording_finished.connect(_on_recording_finished)
+
+func _on_recording_finished(recording_data: Dictionary):
+	load_data = recording_data.duplicate()
+	start_playback()
+
+func start_playback():
+	is_playing = true
+	count = 0
+	playback_timer = 0.0
+	print("Clone starting playback...")
+
+func _physics_process(_delta):
+	if is_playing:
+		get_recording()
+
+func get_recording():
+	count += 1
+	var test = load_data.get(str(count))
+	if test != null:
+		ani.play(test[0])
+		global_position = str_to_var("Vector2" + str(test[1]))
+		ani.flip_h = test[2]
+	else:
+		# End of recording data
+		count = 0
+		print("Clone playback finished, repeating steps")
+	
+	
