ebef91c9d9a7915883a6d2efd34e5a1525c74657
Curio <rayen159nasraoui@outlook.com>
2025-08-02 18:33:40 +0100

    one way collision for clone + added first level

--- scripts/RecordingController.gd
+++ scripts/RecordingController.gd
@@ -16,18 +16,38 @@ var motion = Vector2.ZERO
 var UP : Vector2 = Vector2(0,-1)
 
 var currentClone : Clone
+var selected_clone_index : int = 0
+
 @onready var ani = $AnimatedSprite2D
 
 ##Period for recording player's actions in seconds
 @export_range(1, 10) var recordingDuration: float = 3.0
+@export var cloneArray: Array[Clone]
 
 func _ready():
 	super._ready()
 	# Add player to group so clones can find it
 	add_to_group("player")
 
+	# Set current clone to first available, or null if none
+	if cloneArray.size() > 0:
+		selected_clone_index = 0
+		currentClone = cloneArray[selected_clone_index]
+		print("Found ", cloneArray.size(), " clones. Selected clone: ", selected_clone_index + 1)
+	else:
+		currentClone = null
+		print("No clones found in scene")
+
+func select_next_clone():
+	selected_clone_index = (selected_clone_index + 1) % cloneArray.size()
+	currentClone = cloneArray[selected_clone_index]
+	print("Selected clone: ", selected_clone_index + 1, " of ", cloneArray.size())
+
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
+	
+	if Input.is_action_just_pressed("select_clone"):
+		select_next_clone()
 	
 	if Input.is_action_just_pressed("record") and not is_recording:
 		start_recording()
@@ -41,16 +61,16 @@ func _physics_process(delta: float) -> void:
 			stop_recording()
 
 func start_recording():
-	is_recording = true
 	recording_timer = 0.0
 	count = 0
 	save_data.clear()
+	is_recording = true
 	print("Recording started...")
 
 func stop_recording():
 	is_recording = false
-	print("Recording finished! Sending data to clone...")
-	recording_finished.emit(save_data)
+	print("Recording finished!")
 	
-	# Reset for next recording
-	recording_timer = 0.0
+	# Send signal only to the currently selected clone
+	if currentClone != null and is_instance_valid(currentClone):
+		currentClone._on_recording_finished(save_data)
--- scripts/clone/Clone.gd
+++ scripts/clone/Clone.gd
@@ -14,6 +14,9 @@ var play_reverse = false
 var player_reference = null
 
 func _ready():
+	# Add clone to group so player can find it
+	add_to_group("clones")
+	
 	# Connect to the player's recording controller signal
 	var player = get_tree().get_first_node_in_group("player")
 	if player and player.has_signal("recording_finished"):
@@ -35,35 +38,7 @@ func start_playback():
 func _physics_process(_delta):
 	if is_playing:
 		get_recording()
-		check_player_crushing()
 
-func check_player_crushing():
-	if player_reference == null or not is_instance_valid(player_reference):
-		return
-	
-	var dot_product_vertical = global_position.direction_to(player_reference.global_position).dot(Vector2.RIGHT)
-	var dot_product_horizontal = global_position.direction_to(player_reference.global_position).dot(Vector2.UP)
-
-	var distance = global_position.distance_to(player_reference.global_position)
-	var vector_player_clone = global_position - player_reference.global_position
-
-	var horizontal_crush = distance < 15 and abs(dot_product_horizontal) < .22 and abs(vector_player_clone.y) < 20
-	var vertical_crush = distance < 50 and abs(dot_product_vertical) < .22 and vector_player_clone.y < 0
-	
-	if horizontal_crush or vertical_crush or distance < 0.001:
-
-		var space_state = get_world_2d().direct_space_state
-		var query = PhysicsShapeQueryParameters2D.new()
-		query.shape = collision.shape
-		query.transform = global_transform
-		query.collision_mask = player_reference.collision_layer
-		
-		var results = space_state.intersect_shape(query)
-		for result in results:
-			if result.collider == player_reference:
-				print("Clone is crushing the player! Destroying player...")
-				destroy_player()
-				return
 
 func destroy_player():
 	if player_reference and is_instance_valid(player_reference):
