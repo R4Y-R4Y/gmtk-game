c483e291a2f771b28dc47cfce7e99009be257fca
Marwen <marwen.jlassi.2020@gmail.com>
2025-08-02 18:39:27 +0100

    add pause menu

---
+++ scripts/UI/Pause.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public partial class Pause : CanvasLayer
+{
+    private CanvasLayer pauseMenu;
+
+    public override void _Ready()
+    {
+        pauseMenu = GetNode<CanvasLayer>("PauseMenu");
+        pauseMenu.Visible = false;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("ui_cancel"))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    private void TogglePauseMenu()
+    {
+        bool isPaused = !pauseMenu.Visible;
+        pauseMenu.Visible = isPaused;
+        GetTree().Paused = isPaused;
+
+        pauseMenu.ProcessMode = Node.ProcessModeEnum.Always;
+    }
+
+    public void OnResumePressed()
+    {
+        pauseMenu.Visible = false;
+        GetTree().Paused = false;
+    }
+
+    public void OnRestartPressed()
+    {
+        GetTree().Paused = false;
+        GetTree().ReloadCurrentScene();
+    }
+
+    public void OnBackPressed()
+    {
+        GetTree().Paused = false;
+        GetTree().ChangeSceneToFile("res://scenes/UI/MainMenu.tscn");
+    }
+}
