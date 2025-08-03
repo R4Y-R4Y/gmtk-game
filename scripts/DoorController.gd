b0b1f1ce026897bbafb375cbc2bfb9294ce90beb
Curio <rayen159nasraoui@outlook.com>
2025-08-03 13:43:24 +0100

    fixed door script

--- scripts/DoorController.gd
+++ scripts/DoorController.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var animatedSprite = $AnimatedSprite2D
-
+@onready var exitArea = $ExitArea
 
 @export var ui_win_scene_path: String = "res://scenes/UI/UiWin.tscn"
 @export var door_enabled : bool
@@ -13,17 +13,20 @@ func _ready():
 		animatedSprite.play("open")
 	else:
 		animatedSprite.play("close_idle")
-		
+
+
 func _enable_door():
 	open_door()
 	
 func _disable_door():
 	close_door()
 
-func _on_player_entered(body):
-	if body.is_in_group("player") and door_enabled:
-		instantiate_ui_win()
-		print("Player entered door area - opening door")
+func _process(delta: float) -> void:
+	if door_enabled:
+		var overlapping_bodies = exitArea.get_overlapping_bodies()
+		for body in overlapping_bodies:
+			if body.is_in_group("player"):
+				instantiate_ui_win()
 
 func _on_player_exited(body):
 	if body.is_in_group("player"):
